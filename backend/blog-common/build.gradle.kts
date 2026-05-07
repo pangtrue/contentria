9fc2f3e6cc8d565f41a32adfa9dd89648dffc6f1
이승호 <seung7642@gmail.com>
2026-05-08 00:53:19 +0900

    refactor: kubernetes manifests

--- backend/blog-common/build.gradle.kts
+++ backend/blog-common/build.gradle.kts
@@ -47,3 +47,11 @@ tasks.test {
         }
     }
 }
+
+tasks.named<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+    enabled = false
+}
+
+tasks.named<Jar>("jar") {
+    enabled = true
+}
